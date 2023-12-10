02937260c853ce4227b112d1d2f7280a1fb0bfcb
nathanwolski <45320308+nathanwolski@users.noreply.github.com>
2023-12-09 21:53:14 -0800

    File cleanup

--- output.py
+++
@@ -1,9 +0,0 @@
-#managing output
-import subprocess
-
-def getOutput():
-    results = []
-    for i in range(4):
-        output = subprocess.check_output(f"docker exec container_{i} cat /app/output.txt", shell = True)
-        results.append(output.decode().strip())
-    return combinedResults
